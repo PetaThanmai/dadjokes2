bdf8136c852c924949d9e4d1e1b3a1e2f44ab22d
Peta Thanmai <petathanmai1999@gmail.com>
2022-11-10 13:09:41 +0530

    first commit

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <SearchJokes v-on:search-text="searchText" />
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
+  </div>
+</template>
+
+<script>
+
+import Joke from "../../components/Joke";
+import SearchJokes from "../../components/SearchJokes";
+
+export default defineComponen({
+  components: {
+    Joke,
+    SearchJokes
+  },
+  data() {
+    return {
+      jokes: []
+    };
+  },
+
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+});
+</script>
+
+<style>
+</style>
